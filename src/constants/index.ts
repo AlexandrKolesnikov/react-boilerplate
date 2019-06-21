10c3d20899748f7f6594b6d92c22400d1edeaeff
Alexandr Kolesnikov <a.exellero@gmail.com>
2019-06-21 15:36:53 +0300

    Removed not needed code

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,31 +1,8 @@
 export const { API_URL } = process.env;
 
-export const DATE_FORMAT_SHORT = 'MM/DD/YYYY';
-export const EXPIRATION_PERIOD_UNIT = 'Hours';
-export const CURRENCY = '$';
-export const PAGINATION_ITEMS_PER_PAGE = 20;
-
 export const LOCAL_STORAGE_KEYS = {
   accessToken: 'accessToken',
   accessTokenExpirationDate: 'accessTokenExpirationDate',
   refreshToken: 'refreshToken',
   roles: 'roles',
 };
-
-export const TIMESPAN_MAP_KEYS: { [key: string]: string } = {
-  years: 'y',
-  months: 'm',
-  days: 'd',
-  hours: 'h',
-  minutes: 'min',
-};
-
-export const TIMESPAN_MAP_KEYS_ORDER = [
-  'minutes',
-  'hours',
-  'days',
-  'months',
-  'years',
-];
-
-export const NOT_DIGIT = /[^0-9.]/;
--- src/services/index.ts
+++
@@ -1,7 +0,0 @@
-import _mapValues from 'lodash/mapValues';
-
-export const bindFunctionsByArgument = (functions: any[], data: any) => {
-  const bindActionCreator = (actionCreator: (args?: any[]) => any) => actionCreator.bind(null, data);
-
-  return _mapValues(functions, bindActionCreator);
-};
