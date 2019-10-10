86301c871fa7e02e6eb1d705fbf8f9015e36e511
Alexandr Kolesnikov <a.exellero@gmail.com>
2019-10-10 10:49:05 +0300

    Basic <App /> tests

---
+++ src/modules/app/components/__tests__/index.test.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from '../index';
+
+describe('App Module/Components', () => {
+  it('<App /> should be rendered', () => {
+    const component = shallow(<App />);
+
+    expect(component).toMatchSnapshot();
+  });
+
+  it('<App /> should be rendered with children', () => {
+    const component = shallow(<App>Test</App>);
+
+    expect(component).toMatchSnapshot();
+  });
+});
