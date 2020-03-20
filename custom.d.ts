b9f2378c03856afdf8250bd21eb480be4f68d940
Alexandr Kolesnikov <a.exellero@gmail.com>
2020-03-20 09:47:32 +0200

    custom.d.ts updated to be able to import *.json

--- custom.d.ts
+++ custom.d.ts
@@ -4,3 +4,14 @@ declare module '*.svg' {
   const value: SvgComponent;
   export default value;
 }
+
+declare module '*.json' {
+  const value: any;
+  export default value;
+}
+
+declare namespace NodeJS {
+  interface ProcessEnv {
+    NODE_ENV: string
+  }
+}
