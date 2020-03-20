b56485f4b746c895f46caff9cd88cada8f50cb2f
Alexandr Kolesnikov <a.exellero@gmail.com>
2020-03-20 10:09:15 +0200

    Tests setup with .env.test

--- setupTests.ts
+++ setupTests.ts
@@ -1,4 +1,10 @@
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import dotenv from 'dotenv';
+import path from 'path';
+
+const environmentFilePath = path.resolve(__dirname, './environments/.env.test');
+
+dotenv.config({ path: environmentFilePath });
 
 Enzyme.configure({ adapter: new Adapter() });
