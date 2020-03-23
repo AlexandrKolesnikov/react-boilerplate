ffeaacafbd1cd58c481f64ec15b70081add9ca2b
Alexandr Kolesnikov <a.exellero@gmail.com>
2020-03-23 09:11:14 +0200

    TypeScript images import declarations

--- custom.d.ts
+++ custom.d.ts
@@ -2,14 +2,45 @@ interface SvgComponent extends React.FunctionComponent<React.SVGAttributes<SVGEl
 
 declare module '*.svg' {
   const value: SvgComponent;
+
+  export default value;
+}
+
+declare module '*.png' {
+  const value: string;
+
+  export default value;
+}
+
+declare module '*.jpg' {
+  const value: string;
+
+  export default value;
+}
+
+declare module '*.jpeg' {
+  const value: string;
+
+  export default value;
+}
+
+declare module '*.gif' {
+  const value: string;
+
+  export default value;
+}
+
+declare module '*.bmp' {
+  const value: string;
+
   export default value;
 }
 
 declare module '*.json' {
   const value: any;
+
   export default value;
 }
-
 declare namespace NodeJS {
   interface ProcessEnv {
     NODE_ENV: string
